c2285c0feb13abaf4e8f2a59b754cf2fc7ea1d7b
Caillou <igorlacroute@gmail.com>
2017-11-24 21:39:55 +0000

    make max_results an argument of search methods

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -5,7 +5,7 @@ class ChannelsController < ApplicationController
         youtube_api = YoutubeApiConnector.new
         @search_input = channels_params["search_input"]
         if @search_input.present?
-            @channels = youtube_api.search_channels(@search_input)
+            @channels = youtube_api.search_channels(@search_input, 50)
         else
            @channels = [] 
         end
@@ -14,7 +14,7 @@ class ChannelsController < ApplicationController
     def show
         youtube_api = YoutubeApiConnector.new
         @channel = youtube_api.get_channel_infos(@channel_id)
-        @playlist_items = youtube_api.get_playlist_items(@channel)
+        @playlist_items = youtube_api.get_playlist_items(@channel, 25)
         @videos_stats = youtube_api.get_videos_stats(@playlist_items)
         @last_views_count = @videos_stats.map{ |stats| Integer(stats["statistics"]["viewCount"]) }.sum
         @last_likes_count = @videos_stats.map{ |stats| Integer(stats["statistics"]["likeCount"]) }.sum
--- app/models/youtube_api_connector.rb
+++ app/models/youtube_api_connector.rb
@@ -4,36 +4,34 @@ class YoutubeApiConnector
     API_KEY = ENV["GOOGLE_API_KEY"]
     OAUTH_CLIENT_ID = ENV["GOOGLE_CLIENT_ID"]
     OAUTH_CLIENT_SECRET = ENV["GOOGLE_CLIENT_SECRET"]
-        
-    MAX_RESULTS = 10
     
     def initialize()
         initialize_secret_keys()
     end
     
-    def search_channels(search_str)
+    def search_channels(search_str, max_results)
         url = "https://www.googleapis.com/youtube/v3/search?part=snippet&type=channel&q=#{URI::encode(search_str)}"
-        items = get_resource_first_page(url)
+        items = get_resource_first_page(url, max_results)
         return items
     end
     
     def get_channel_infos(channel_id)
         url = "https://www.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=#{channel_id}"
-        items = get_resource_first_page(url).first
+        items = get_resource_first_page(url, 1).first
         return items
     end
     
-    def get_playlist_items(channel)
+    def get_playlist_items(channel, max_results)
         playlist_id = channel["contentDetails"]["relatedPlaylists"]["uploads"]
         url = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&playlistId=#{playlist_id}"
-        items = get_resource_first_page(url)
+        items = get_resource_first_page(url, max_results)
         return items
     end
     
     def get_videos_stats(playlist_items)
         videos_ids = playlist_items.map{ |playlist_item| playlist_item["contentDetails"]["videoId"] }.join(",")
         url = "https://www.googleapis.com/youtube/v3/videos?part=statistics&id=#{videos_ids}"
-        items = get_resource_first_page(url)
+        items = get_resource_first_page(url, playlist_items.count)
         return items
     end
 
@@ -43,15 +41,15 @@ class YoutubeApiConnector
             get_resource_page(url, nil)
         end
         
-        def get_resource_first_page(url)
-            full_url = URI.parse("#{url}&maxResults=#{MAX_RESULTS}&key=#{API_KEY}")
+        def get_resource_first_page(url, max_results)
+            full_url = URI.parse("#{url}&maxResults=#{max_results}&key=#{API_KEY}")
             res = Net::HTTP.get(full_url)
             json = ActiveSupport::JSON.decode(res)
             json["items"]
         end
         
-        def get_resource_page(url, page_token)
-            full_url = URI.parse("#{url}&maxResults=#{MAX_RESULTS}&key=#{API_KEY}&pageToken=#{page_token}")
+        def get_resource_page(url, max_results, page_token)
+            full_url = URI.parse("#{url}&maxResults=#{max_results}&key=#{API_KEY}&pageToken=#{page_token}")
             res = Net::HTTP.get(full_url)
             json = ActiveSupport::JSON.decode(res)
             items = json["items"]
