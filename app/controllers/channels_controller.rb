165d8884a3d5914e20781d825065c039d7b7b7c2
Caillou <igorlacroute@gmail.com>
2017-11-24 17:05:05 +0000

    display channel statistics in show page

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,4 +1,6 @@
 class ChannelsController < ApplicationController
+    before_action :set_channel_id, only: [:show]
+    
     def search_channels
         youtube_api = YoutubeApiConnector.new
         @search_input = channels_params["search_input"]
@@ -10,10 +12,15 @@ class ChannelsController < ApplicationController
     end
     
     def show
-        
+        youtube_api = YoutubeApiConnector.new
+        @channel = youtube_api.get_channel_infos(@channel_id)
     end
     
     private
+    
+        def set_channel_id
+           @channel_id = params["id"] 
+        end
     
         def channels_params
            params.permit("search_input")
--- app/models/youtube_api_connector.rb
+++ app/models/youtube_api_connector.rb
@@ -3,24 +3,41 @@ class YoutubeApiConnector
     OAUTH_CLIENT_ID = ENV["GOOGLE_CLIENT_ID"]
     OAUTH_CLIENT_SECRET = ENV["GOOGLE_CLIENT_SECRET"]
         
+    MAX_RESULTS = 50
+    
     def initialize()
         initialize_secret_keys()
     end
     
     def search_channels(search_str)
-        url = "https://www.googleapis.com/youtube/v3/search?part=snippet&type=channel&q=#{search_str}"
-        ap url
-        get_resource(url)
+        url = "https://www.googleapis.com/youtube/v3/search?part=snippet&type=channel&q=#{URI::encode(search_str)}"
+        items = get_resource_first_page(url)
+        ap items
+        items
     end
     
+    def get_channel_infos(channel_id)
+        url = "https://www.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=#{channel_id}"
+        items = get_resource_first_page(url).first
+        ap items
+        items
+    end
+
     private
     
-        def get_resource(url)
+        def get_resource_all_pages(url)
             get_resource_page(url, nil)
         end
         
+        def get_resource_first_page(url)
+            full_url = URI.parse "#{url}&maxResults=#{MAX_RESULTS}&key=#{API_KEY}"
+            res = Net::HTTP.get(full_url)
+            json = ActiveSupport::JSON.decode(res)
+            json["items"]
+        end
+        
         def get_resource_page(url, page_token)
-            full_url = URI.parse "#{url}&maxResults=50&key=#{API_KEY}&pageToken=#{page_token}"
+            full_url = URI.parse "#{url}&maxResults=#{MAX_RESULTS}&key=#{API_KEY}&pageToken=#{page_token}"
             res = Net::HTTP.get(full_url)
             json = ActiveSupport::JSON.decode(res)
             items = json["items"]
